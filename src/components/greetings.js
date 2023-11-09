6b0c587c60aaf4977489a7af8efb74c40e7081a5
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-11-09 12:22:40 +0200

    Modify component file

--- src/components/greetings.js
+++ src/components/greetings.js
@@ -1,21 +1,28 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { fetchGreetingObj } from '../redux/greetings/greetings';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRandomGreeting } from '../redux/greetings/greetings';
 
 function Greeting() {
   const dispatch = useDispatch();
+
   useEffect(() => {
-    dispatch(fetchGreetingObj());
+    dispatch(fetchRandomGreeting());
   }, [dispatch]);
 
   const { greeting } = useSelector((state) => state.greetings);
+
   const generate = () => {
-    dispatch(fetchGreetingObj());
+    dispatch(fetchRandomGreeting());
   };
+
+  // Check if greeting is an object and has greeting_text property
+  const greetingText = typeof greeting === 'object' && greeting.greeting_text
+    ? greeting.greeting_text
+    : 'Invalid Greeting';
 
   return (
     <div className="greeting-content">
-      <h1>{greeting}</h1>
+      <h1>{greetingText}</h1>
       <button onClick={generate} type="button">Generate new greeting</button>
     </div>
   );
